14fdd1733bcb9c28f2121090b6daffe864bbfa04
gjspencer22 <gjspencer22@gmail.com>
2021-07-08 04:33:28 -0600

    pushing

--- db/schema.sql
+++ db/schema.sql
@@ -3,11 +3,10 @@ CREATE DATABASE employee;
 
 USE employee;
 
-CREATE TABLE employee(
-    id INTEGER NOT NULL,
-    book_name NOT NULL
-    first_name VARCHAR(20) NOT NULL,
-    LAST_NAME VARCHAR(20) NOT NULL
+CREATE TABLE employees(
+    id INTEGER NOT NULL,
+    book_name VARCHAR(20) NOT NULL,
+    first_name VARCHAR(20) NOT NULL,
+    LAST_NAME VARCHAR(20) NOT NULL
 );
-
 
